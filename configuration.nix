28cdefa2622d6b265b238a072fe68c3c71e769be
Joe Pea <joe@trusktr.io>
2020-01-18 00:45:09 -0800

    format and remove comments from config

--- configuration.nix
+++ configuration.nix
@@ -2,36 +2,28 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
-
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+{ config, pkgs, ... }: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "starnix"; # Define your hostname. # CUSTOMIZED
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # ADDED
+  networking = {
+    hostName = "starnix";
+    networkmanager.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  # CUSTOMIZED, disabled because we added networkmanager above which does the configuration.
-  #networking.useDHCP = false;
-  #networking.interfaces.enp0s31f6.useDHCP = true;
-  #networking.interfaces.wlp1s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    networking.firewall.enable = false;
+  };
 
   # Select internationalisation properties.
-  # CUSTOMIZED
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
@@ -39,10 +31,9 @@
   };
 
   # Set your time zone.
-  # CUSTOMIZED
   time.timeZone = "America/Los_Angeles";
 
-  # ADDED, config for certain packages
+  # config for certain packages
   nixpkgs.config = {
     allowUnfree = true;
 
@@ -60,7 +51,6 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search <term>
-  # CUSTOMIZED
   environment.systemPackages = with pkgs; [
     nixfmt
 
@@ -97,7 +87,6 @@
     vscodium
   ];
 
-  #ADDED
   programs.zsh.enable = true;
   programs.chromium.enable = true;
 
@@ -109,50 +98,45 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # CUSTOMIZED
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false; # CUSTOMIZED
-
   # Enable CUPS to print documents.
-  services.printing.enable = true; # CUSTOMIZED
+  services.printing.enable = true;
 
   # Enable sound.
-  # CUSTOMIZED
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true; # CUSTOMIZED
-  services.xserver.layout = "us"; # CUSTOMIZED
-  # services.xserver.xkbOptions = "eurosign:e";
-  services.xserver.xkbOptions = "ctrl:swapcaps"; # ADDED
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbOptions = "ctrl:swapcaps";
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true; # CUSTOMIZED
+    # Enable touchpad support.
+    libinput.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
+    # Enable the KDE Desktop Environment.
+    # displayManager.sddm.enable = true;
+    # desktopManager.plasma5.enable = true;
 
-  # Enable the Gnome Desktop Environment. https://nixos.wiki/wiki/Gnome
-  # TODO try Wayland instead of X11. See here: https://github.com/NixOS/nixpkgs/issues/32806#issuecomment-449987334
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-  services.xserver.desktopManager.gnome3.enable = true;
+    # Enable the Gnome Desktop Environment. https://nixos.wiki/wiki/Gnome
+    # TODO try Wayland instead of X11. See here: https://github.com/NixOS/nixpkgs/issues/32806#issuecomment-449987334
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = false;
+    desktopManager.gnome3.enable = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # CUSTOMIZED
   users = {
     users = {
       trusktr = {
         isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" "adbusers" ]; # Enable ‘sudo’ for the user.
-        #shell = pkgs.zsh;
+        extraGroups = [
+	  "wheel" # Enable ‘sudo’ for the user.
+	  "networkmanager"
+	  "adbusers" # access to Android devices
+	];
       };
     };
     defaultUserShell = pkgs.zsh;
@@ -164,6 +148,5 @@
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "19.09"; # Did you read the comment?
-
 }
 
