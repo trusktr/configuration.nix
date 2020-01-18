705e21d9065dfd1abbbcf94f56049c63ce50ec08
Joe Pea <joe@trusktr.io>
2020-01-18 00:46:30 -0800

    fix mistake

--- configuration.nix
+++ configuration.nix
@@ -17,10 +17,10 @@
     networkmanager.enable = true;
 
     # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
     # Or disable the firewall altogether.
-    networking.firewall.enable = false;
+    firewall.enable = false;
   };
 
   # Select internationalisation properties.
