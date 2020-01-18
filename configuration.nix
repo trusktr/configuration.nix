9d7ec720d6b9d57035e5b58a8984614842f4bd0e
Joe Pea <joe@trusktr.io>
2020-01-17 19:51:06 -0800

    disable nixos network options, as the networkmanager option will manage it instead

--- configuration.nix
+++ configuration.nix
@@ -21,9 +21,10 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s31f6.useDHCP = true;
-  networking.interfaces.wlp1s0.useDHCP = true;
+  # CUSTOMIZED, disabled because we added networkmanager above which does the configuration.
+  #networking.useDHCP = false;
+  #networking.interfaces.enp0s31f6.useDHCP = true;
+  #networking.interfaces.wlp1s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
